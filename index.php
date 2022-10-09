4645b0bf6b1fdd4e7c7f2c36399002e90e3673be
unknown <luka.jeremic@acme.co.me>
2022-10-09 16:15:50 +0200

    first

---
+++ index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" /> 
+  <meta name="author" content="" />
+
+  <title>luka media</title>
+
+  <!-- slider stylesheet -->
+  <!-- <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" /> -->
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <span>
+              luka media
+            </span>
+          </a>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="s-1"> </span>
+            <span class="s-2"> </span>
+            <span class="s-3"> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#about_section"> About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#service_section"> Service </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#portfolio_section"> Portfolio </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#contact_section">Contact </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+    <!-- slider section -->
+    <section class=" slider_section ">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6 ">
+                  <div class="detail_box">
+                    <h1>
+						We Help Your Businesses Produce Extraordinary<br>
+						ROI Via <span>ADVERTISING</span>
+                    </h1>
+                    <p>
+                      Stop wasting money on ineffective campaigns. It's time to scale your business and revolutionize your brand.
+                    </p>
+                    <a href="" class="">
+                      Schedule your free call
+                    </a>
+                  </div>
+                </div>
+                <div class="col-md-5 offset-md-1">
+                  <div class="img-box">
+                    <img src="images/dddd.png" alt="">
+                    <!-- <img src="images/slider-img.png" alt=""> -->
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- <div class="carousel-item ">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6 ">
+                  <div class="detail_box">
+                    <h1>
+                      WE HELP YOU TO MAKE <br>
+                      BETTER <span>SOFTWARE</span>
+                    </h1>
+                    <p>
+                      It is a long established fact that a reader will be distracted by the readable content of a page
+                      when looking
+                    </p>
+                    <a href="" class="">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+                <div class="col-md-5 offset-md-1">
+                  <div class="img-box">
+                    <img src="images/Digital-Marketing-2.png" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel_btn-container">
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="sr-only">Next</span>
+          </a>
+        </div> -->
+      </div>
+    </section>
+    <!-- end slider section -->
+  </div>
+
+
+  <!-- service section -->
+  <section class="service_section layout_padding">
+    <div class="container" id="service_section">
+      <div class="heading_container">
+        <h2>
+          Our Services
+        </h2>
+      </div>
+
+      <div class="service_container">
+        <div class="box">
+          <div class="img-box">
+            <img src="images/s-1.png" class="img1" alt="">
+            <img src="images/s-1-color.png" class="img2" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              Growing Sales
+            </h5>
+            <p>
+              Unique perspective of analytics and growth of your customers and sales, day by day.
+            </p>
+          </div>
+        </div>
+        <div class="box active">
+          <div class="img-box">
+            <img src="images/s-2.png" class="img1" alt="">
+            <img src="images/s-2-color.png" class="img2" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              Web Development
+
+            </h5>
+            <p>
+             Your website which will give you an online presence and boost your sales.
+            </p>
+          </div>
+        </div>
+        <div class="box">
+          <div class="img-box">
+            <img src="images/s-3.png" class="img1" alt="">
+            <img src="images/s-3-color.png" class="img2" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              Digital Marketing
+            </h5>
+            <p>
+				Your business wil get people talking.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="btn-box">
+        <a href="">
+          Read More
+        </a>
+      </div>
+    </div>
+  </section>
+  <!-- end service section -->
+
+  <!-- about section -->
+  <section class="about_section layout_padding">
+    <div class="container-fluid" id="about_section">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="img-box">
+            <img src="images/about-img.png" alt="" />
+          </div>
+        </div>
+        <div class="col-md-5">
+          <div class="detail-box">
+            <div class="heading_container">
+              <h2>
+                About Us
+              </h2>
+            </div>
+            <p>
+              We cultivate relationships with clients and together we grow businesses. We specialize in digital marketing and web design.
+			  Your company needs an online presence and we are here to help. 
+			  
+            </p>
+            <a href="mailto:lukajeremic21@gmail.com">
+              Get in touch
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end about section -->
+
+
+
+<section>
+	<div class="container col-xxl-8 px-4 py-5">
+		<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+			<div class="col-lg-6">
+				<h1 class="display-5 fw-bold lh-1 mb-3">Let's talk</h1>
+				<p class="lead">By the end of this Audit call, you will have a clear understanding of the next 
+					steps you can take for your business to start generating consistent and reliable results online with our services.</p>
+				<div class="d-grid gap-2 d-md-flex justify-content-md-start">
+					<a href="mailto:lukajeremic21@gmail.com" class="zoom_meeting">
+						Schedule a zoom meeting
+					</a>
+				</div>
+			  </div>
+		  <div class="col-10 col-sm-8 col-lg-6">
+			<img src="images/profile.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" style="border-radius: 50%;" width="300" height="300" loading="lazy">
+		  </div>
+		</div>
+	  </div>
+</section>
+
+  <div class="footer_bg">
+
+
+
+    <!-- info section -->
+    <section class="info_section ">
+      <div class="container" id="info_section">
+        <div class="row">
+          <div class="col">
+            <div class="info_info">
+              <h5>
+                Let's talk
+              </h5>
+              <p>
+              If you have a general inquiry and would like to speak to us, you can contact us via email at: lukajeremic21@gmail.com
+              </p>
+            </div>
+          </div>
+          <div class="col">
+            <div class="info_info">
+              <h5>
+                Social
+              </h5>
+
+			  <div class="row">
+
+			  	<div class="col-2">
+
+					<a href="https://www.flaticon.com/free-icons/facebook" title="facebook icons"><img src="images/facebook.png" alt="Facebook" style="width:42px;height:42px;"></a>
+
+				</div>
+
+				<div class="col-2">
+					<a href="https://www.flaticon.com/free-icons/facebook" title="facebook icons"><img src="images/linkedin.png" alt="Facebook" style="width:42px;height:42px;"></a>
+				</div>
+
+
+				<div class="col-2">
+					<a href="https://www.flaticon.com/free-icons/facebook" title="facebook icons"><img src="images/instagram.png" alt="Facebook" style="width:42px;height:42px;"></a>
+				</div>
+
+
+				</div>
+			  </div>
+
+            </div>
+          </div>
+        </div>
+        <br><br>
+
+        <div class="row">
+          <div class="col-lg-7 col-md-9 mx-auto">
+            <p>
+              &copy; <?php echo date("Y"); ?> All Rights Reserved By
+              LUKA MEDIA
+            </p>
+          </div>
+        </div>
+  
+
+      </div>
+    </section>
+
+    <!-- end info_section -->
+
+
+
+  </div>
+
+
+
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
